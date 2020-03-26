b71092f5512c40d24c7480a5225ec341bc23c342
Fabrizio Fiore <fabrizio.fiore.91@gmail.com>
2020-03-26 12:05:07 +0100

    Nuova tabella changes

---
+++ db/NuovoDb_26-03/sampledb_conversion.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: sampledb
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `conversion`
+--
+
+DROP TABLE IF EXISTS `conversion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `conversion` (
+  `idConversion` int NOT NULL AUTO_INCREMENT,
+  `source` varchar(500) NOT NULL,
+  `sourceType` varchar(5) NOT NULL,
+  `outputType` varchar(5) NOT NULL,
+  `idUser` int NOT NULL,
+  `changes` tinyint NOT NULL,
+  PRIMARY KEY (`idConversion`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `conversion`
+--
+
+LOCK TABLES `conversion` WRITE;
+/*!40000 ALTER TABLE `conversion` DISABLE KEYS */;
+INSERT INTO `conversion` VALUES (1,'<note><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Don\'tforgetmethisweekend!</body></note>','xml','json',0,0),(2,'jhgfdsa','xml','json',0,0),(3,'<note><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Don\'tforgetmethisweekend!</body></note>','xml','json',0,0);
+/*!40000 ALTER TABLE `conversion` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-03-26 12:04:24
